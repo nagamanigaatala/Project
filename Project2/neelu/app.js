3c8926076c2ffcc6b8e99f7bb3d541fdf7d5e5a1
Nagamani Gaatala <57241062+nagamanigaatala@users.noreply.github.com>
2023-12-09 20:48:05 +0530

    Update app.js

--- Project2/neelu/app.js
+++ Project2/neelu/app.js
@@ -1,1 +1,15 @@
+var app = angular.module('tollCalculator', ['ui.router']);
 
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+  .state('home',{
+    url: '/',
+    templateUrl: 'public/views/partials/home.html',
+    controller: 'mainController'
+  })
+
+  $locationProvider.html5Mode(true);
+}]);
